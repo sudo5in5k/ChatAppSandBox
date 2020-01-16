761ce0f95ddfeaee13584095b2f7615625089bf8
ussy <sudo5in5k@gmail.com>
2020-01-17 01:35:04 +0900

    add libraries

---
+++ app/src/main/java/com/example/chatappsandbox/ui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.chatappsandbox.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.chatappsandbox.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+
+    }
+}
