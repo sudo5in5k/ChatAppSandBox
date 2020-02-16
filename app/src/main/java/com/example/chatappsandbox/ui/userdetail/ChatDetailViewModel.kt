dc48111db60f56d9d5cb18c82cea6461e0f07b37
ussy <sudo5in5k@gmail.com>
2020-02-16 11:20:46 +0900

    add 2way databinding impl when post message

--- app/src/main/java/com/example/chatappsandbox/ui/userdetail/ChatDetailViewModel.kt
+++ app/src/main/java/com/example/chatappsandbox/ui/userdetail/ChatDetailViewModel.kt
@@ -1,8 +1,9 @@
 package com.example.chatappsandbox.ui.userdetail
 
 import android.app.Application
-
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.chatappsandbox.entity.Message
@@ -12,12 +13,20 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
-class ChatDetailViewModel(private val app: Application) : AndroidViewModel(app) {
+class ChatDetailViewModel(app: Application) : AndroidViewModel(app) {
 
     private val db = FirebaseDatabase.getInstance()
     val isLoading = MutableLiveData<Boolean>(false)
-    val sendClickable = MutableLiveData<Boolean>(false)
     val allArchives = MutableLiveData<List<Message>>()
+
+    val draft = MutableLiveData<String?>()
+    val sendClickable = MediatorLiveData<Boolean>()
+
+    init {
+        sendClickable.addSource(draft) {
+            sendClickable.value = it?.isNotEmpty() ?: false
+        }
+    }
 
     private fun performLoading() {
         isLoading.postValue(true)
@@ -28,6 +37,7 @@ class ChatDetailViewModel(private val app: Application) : AndroidViewModel(app) 
         uid ?: return
         message ?: return
         performLoading()
+        //TODO ２つとってきたのち整列させる
         val readDataFlow = db.getReference("messages/$uid").fetchMessageArchiveWithFlow()
         viewModelScope.launch {
             readDataFlow.collect {
@@ -40,6 +50,7 @@ class ChatDetailViewModel(private val app: Application) : AndroidViewModel(app) 
         isLoading.postValue(false)
     }
 
+
     /**
      * ユーザーからこれまでのアーカイブを特定して返す
      * TODO to repository
@@ -48,6 +59,11 @@ class ChatDetailViewModel(private val app: Application) : AndroidViewModel(app) 
         list.filter { it.from == message.from }.sortedBy { it.time }
 
     fun onSendClick() {
+        Log.d("debug", "send click")
+    }
 
+    override fun onCleared() {
+        super.onCleared()
+        sendClickable.removeSource(draft)
     }
 }
